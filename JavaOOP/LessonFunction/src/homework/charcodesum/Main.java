129a2d0aaf7478b9bbd52529580c0aa2c7509df5
militrik <militrik@gmail.com>
2023-01-17 11:44:39 +0200

    [+] Function. Chars integer sum

--- JavaOOP/LessonFunction/src/homework/charcodesum/Main.java
+++ JavaOOP/LessonFunction/src/homework/charcodesum/Main.java
@@ -6,14 +6,19 @@ public class Main {
     public static void main(String[] args) {
         String text = "Hello world";
 
-        Function<String, Integer> pr1 = s -> {
+        Function<String, char[]> fun1 = String::toCharArray;
+
+        Function<char[], Integer> fun2 = chars -> {
             int sum = 0;
-            for (char element : s.toCharArray()
+            for (char element : chars
             ) {
                 sum = Integer.sum((int) element, sum);
             }
             return sum;
         };
-        System.out.println(pr1.apply(text));
+
+        Function<String, Integer> fun3 = fun2.compose(fun1);
+
+        System.out.println(fun3.apply(text));
     }
 }
