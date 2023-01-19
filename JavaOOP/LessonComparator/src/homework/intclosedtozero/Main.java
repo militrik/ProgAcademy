33f4e8f1a5ff661b286b65d9000f36a174097690
militrik <militrik@gmail.com>
2023-01-19 20:29:32 +0200

    [=] Comparator. int closed to zero, fix

--- JavaOOP/LessonComparator/src/homework/intclosedtozero/Main.java
+++ JavaOOP/LessonComparator/src/homework/intclosedtozero/Main.java
@@ -2,16 +2,21 @@ package homework.intclosedtozero;
 
 import java.util.*;
 
+
 public class Main {
     public static void main(String[] args) {
 
         Random rn = new Random();
-        List<Integer> integerList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-                  integerList.add(rn.nextInt(-10,20 ));
-        }
-        System.out.println(integerList);
+       List<Integer> list = new ArrayList<>(List.of(-2, -2, 2, 5, 7, -5, -7));
 
-        System.out.println( Math.abs(Collections.min(integerList, Comparator.comparingInt(Math::abs))));
+        System.out.println(list);
+
+        Comparator<Integer> comp1 = Comparator.comparingInt(Math::abs);
+        Comparator<Integer> comp2 = Comparator.comparingInt(a -> -a);
+
+        list.sort(comp1.thenComparing(comp2));
+
+        System.out.println(list);
+        System.out.println(list.get(0));
     }
 }
