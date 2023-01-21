12a2c660614bb06455dc262490a9f1754856a04b
militrik <militrik@gmail.com>
2023-01-21 23:01:10 +0200

    [+] Comparator. sort file punkt mark

--- JavaOOP/LessonComparator/src/homework/sortfilepunktmark/Main.java
+++ JavaOOP/LessonComparator/src/homework/sortfilepunktmark/Main.java
@@ -1,15 +1,33 @@
 package homework.sortfilepunktmark;
 
 import java.io.File;
-import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.ToIntFunction;
+
 
 public class Main {
     public static void main(String[] args) {
         File[] files = new File(".").listFiles((file, s) -> s.endsWith(".txt") && !file.isFile());
         List<File> listFiles = Arrays.asList(Objects.requireNonNull(files));
-/*        Comparator<File> comp1 = (o1, o2) -> {
-            o1.
-        };*//*
-        listFiles.sort(comp1);*/
+        System.out.println(listFiles);
+
+        ToIntFunction<File> fun = a -> {
+            String text;
+            try {
+                text = Files.readString(Path.of(a.getPath()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return text.length() - text.replaceAll("[ ,.?!-]", "").length();
+        };
+        Comparator<File> resultComp = Comparator.comparingInt(fun);
+        listFiles.sort(resultComp);
+        System.out.println(listFiles);
     }
 }
